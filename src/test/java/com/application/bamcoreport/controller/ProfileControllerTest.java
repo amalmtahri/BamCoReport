9b9b1b24ce3a4072c02ee2125a0660d27dededb8
KABRANESOUMIA <kabrane.soumia.com>
2022-02-04 07:26:14 +0100

    create tests files

---
+++ src/test/java/com/application/bamcoreport/controller/ProfileControllerTest.java
@@ -0,0 +1,28 @@
+package com.application.bamcoreport.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileControllerTest {
+
+    @Test
+    void saveProfile() {
+    }
+
+    @Test
+    void getAllProfiles() {
+    }
+
+    @Test
+    void findProfile() {
+    }
+
+    @Test
+    void deleteProfile() {
+    }
+
+    @Test
+    void updateProfile() {
+    }
+}
