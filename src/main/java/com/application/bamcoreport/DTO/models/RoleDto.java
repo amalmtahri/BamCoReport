cd7e674f4e687bc8d0d51caf4b57174b7f9f4656
ext_mtahri <mtahriamal0@gmail.com>
2022-02-11 09:05:15 +0100

    update role

--- src/main/java/com/application/bamcoreport/DTO/models/RoleDto.java
+++ src/main/java/com/application/bamcoreport/DTO/models/RoleDto.java
@@ -17,7 +17,7 @@ public class RoleDto {
     private String displayname;
     private String description;
 
-    @JsonManagedReference
+    //@JsonManagedReference
     private User createdby;
     private Date creationdate;
     private Date lastupdate;
